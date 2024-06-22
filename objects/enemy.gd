b15823049327f1940d84a5aa3fbd6596b3bac49b
Amnoshield <asa.j.kramer@gmail.com>
2024-06-22 15:15:43 -0400

    almost done with path finding
    It still needs more work

--- objects/enemy.gd
+++ objects/enemy.gd
@@ -6,22 +6,21 @@ extends CharacterBody2D
 
 
 func _ready():
+	$NavigationAgent2D.max_speed = speed
 	$"Ai update".start(AI_update_speed)
+	_on_ai_update_timeout()
 
 
-func _physics_process(delta):
-	var global_pos = nav.get_next_path_position()
-	var relitive_pos:Vector2 = global_pos-position
-	
-	print(global_pos, relitive_pos, velocity)
+func _physics_process(_delta):
+	var relitive_pos:Vector2 = nav.get_next_path_position()- global_position
+
 	
 	velocity = relitive_pos.limit_length(speed)
 	move_and_slide()
-	
 
 
-func make_path(position:Vector2):
-	nav.set_target_position(position)
+func make_path(_position:Vector2):
+	nav.set_target_position(_position)
 
 
 func _on_ai_update_timeout():
