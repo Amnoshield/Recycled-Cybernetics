5d91dde7da4bbf20dcd12c4d9db5eb3cadb8ab74
Amnoshield <asa.j.kramer@gmail.com>
2025-01-07 22:47:22 -0500

    update to the level gen + IDK what is happening to my pc

--- levels/levels fr fr og/level_gen.gd
+++ levels/levels fr fr og/level_gen.gd
@@ -5,7 +5,7 @@ extends Node2D
 	"type":"start room",
 	"start":Vector2(0, 0),
 	"end":Vector2(0,-1),
-	"rect":Rect2(0,-1,0,2),
+	"rect":Rect2(0,-1,1,2),
 	"direction":"north"
 }]
 
@@ -27,32 +27,27 @@ func line_gen(parent:Dictionary, length_range:Vector2, cont:Dictionary = {}):
 	var start = parent["end"]
 
 	for length_idx in lengths.size():
+		if cont and length_idx < cont["length_idx"]+1: continue
+		
 		var length = lengths[length_idx]
-		if cont and length_idx < cont["length_idx"]+1:
-			continue
-			
 		var directions
 		if cont:
 			directions = cont["directions"]
-		elif parent["type"] == "line":
+		else:
 			match parent["direction"]:
 				"north", "south":
 					directions = ["east", "west"]
 				"east", "west":
 					directions = ["north", "south"]
 				_:
+					directions = ["north", "south", "east", "west"]
 					push_error("Direction not found: "+str(parent["direction"]))
-			directions.shuffle()
-		else:
-			directions = ["north", "south", "east", "west"]
-			
 			directions.shuffle()
 
 		for direction_idx in directions.size():
-			var direction = directions[direction_idx]
-			if cont and direction_idx < cont["direction_idx"] +1:
-				continue
+			if cont and direction_idx < cont["direction_idx"]+1: continue
 			
+			var direction = directions[direction_idx]
 			var end
 			match direction:
 				"north":
@@ -78,6 +73,9 @@ func line_gen(parent:Dictionary, length_range:Vector2, cont:Dictionary = {}):
 				"directions":directions,
 				"direction_idx":direction_idx
 			}
+			if line["start"] != parent["end"]:
+				push_error("HELLP")
+			
 			var reorded_line = reorder_shape(line.duplicate())
 			line["rect"] = Rect2(reorded_line["start"], reorded_line["end"]-reorded_line["start"])
 
@@ -87,37 +85,70 @@ func line_gen(parent:Dictionary, length_range:Vector2, cont:Dictionary = {}):
 
 
 func room_gen(parent:Dictionary, size_rand:Vector2, cont:Dictionary = {}):
-	var plain = func ():
-		return
-
+	var sizes_x
+	var sizes_y
 	if cont:
-		push_error("not done yet")
+		sizes_x = cont["sizes_x"]
+		sizes_y = cont["sizes_y"]
+	else:
+		sizes_x = Array(range(size_rand[0], size_rand[1]+1))
+		sizes_x.shuffle()
+		sizes_y = Array(range(size_rand[0], size_rand[1]+1))
+		sizes_y.shuffle()
 
-	var sizes_x = Array(range(size_rand[0], size_rand[1]+1))
-	sizes_x.shuffle()
-	var sizes_y = Array(range(size_rand[0], size_rand[1]+1))
-	sizes_y.shuffle()
-
-	for x in sizes_x:
-		for y in sizes_y:
-			var size = Vector2(x, y)
+	for x_idx in sizes_x.size():
+		if cont and x_idx < cont["x_idx"]+1: continue
+		
+		var x = sizes_x[x_idx]
+		for y_idx in sizes_y.size():
+			if cont and y_idx < cont["y_idx"]+1: continue
 			
-			var offset_value
+			var y = sizes_y[y_idx]
+			var size = Vector2(x, y)
+			var offset_xy
 			if parent["direction"] in ["north", "south"]:
-				offset_value = x
+				offset_xy = x
 			else:
-				offset_value = y
+				offset_xy = y
 
-			var offsets = Array(range(0, offset_value+1))
-			offsets.shuffle()
+			var offsets
+			if cont:
+				offsets = cont["offsets"]
+			else:
+				offsets = Array(range(0, offset_xy+1))
+				offsets.shuffle()
 
-			print("Offset not implemented yet")
-
-			var start = (parent["start"][0])
-
-			for offset in offsets:
-				var room = {"type":"room", "start":"temp"}
-				pass
+			for offset_idx in offsets:
+				if cont and offset_idx < cont["offset_idx"]+1: continue
+				
+				var offset = offsets[offset_idx]
+				var start
+				if parent["direction"] in ["north", "south"]:
+					start = Vector2(parent["start"][0]-offset, parent["start"][1])
+				else:
+					start = Vector2(parent["start"][0]-offset, parent["start"][1])
+				var end = start+size
+				
+				var room = {
+					"type":"room",
+					"parent":parent,
+					"start":start,
+					"end":end,
+					"sizes_x":sizes_x,
+					"x_idx":x_idx,
+					"sizes_y":sizes_y,
+					"y_idx":y_idx,
+					"offsets":offsets,
+					"offset_idx":offset_idx,
+					"direction":null
+				}
+				
+				var reorded_room = reorder_shape(room.duplicate())
+				room["rect"] = Rect2(reorded_room["start"], reorded_room["end"]-reorded_room["start"])
+				
+				if not detect_collisions(room, plan):
+					return room
+	return false
 
 
 func detect_collisions(shape:Dictionary, other_shapes:Array[Dictionary]):
@@ -141,13 +172,13 @@ func reorder_shape(shape:Dictionary):
 		shape["start"].y = shape["end"].y
 		shape["end"].y = temp
 
-	#shape["end"][0] += 1
-	#shape["end"][1] += 1
+	shape["end"][0] += 1
+	shape["end"][1] += 1
 
 	return shape
 
 
-func gen_main_hall(num_rooms=Vector2(2,4), num_halls=Vector2(1,2), hall_length=Vector2(2,4)):
+func gen_main_hall(num_rooms=Vector2(2,4), room_size=Vector2(3,5), num_halls=Vector2(1,2), hall_length=Vector2(2,4)):
 	var instructions = []
 	for room in range(rng.randi_range(num_rooms[0], num_rooms[1])):
 		for x in range(rng.randi_range(num_halls[0], num_halls[1])):
@@ -163,33 +194,48 @@ func gen_main_hall(num_rooms=Vector2(2,4), num_halls=Vector2(1,2), hall_length=V
 	var total_shapes = len(instructions)
 	var counter = 0
 	while counter < total_shapes:
+		for shape in plan:
+			if shape["type"] == "line":
+				if shape["start"] !=  shape["parent"]["end"]:
+					print("cought you")
+		
+		var last_shape = plan[-1]
+		if last_shape["type"] == "line" and plan[-2]["type"] == "line" and last_shape["start"] != plan[-2]["end"]:
+			print("fuck")
+		elif last_shape["type"] == "line":
+			"idk man"
+		
 		if counter < len(plan)-1:
 			tries += 1
 		match instructions[counter]:
 			"hall":
-				if counter < len(plan)-1:
-					var new_line = line_gen(plan[-1], hall_length, plan.pop_back())
-					if new_line:
-						plan.append(new_line)
-						counter += 1
-					else:
-						counter -= 1
+				var new_line
+				if counter < len(plan)-1: new_line = line_gen(plan[-2], hall_length, plan.pop_back())
+				else: new_line = line_gen(plan[-1], hall_length)
+				
+				if new_line:
+					plan.append(new_line)
+					counter += 1
 				else:
-					var new_line = line_gen(plan[-1], hall_length)
-					if new_line:
-						plan.append(new_line)
-						counter += 1
-					else:
-						counter -= 1
+					counter -= 1
 			"room":
-				counter += 1
-				continue
+				#counter += 1
+				#continue
+				var new_room
+				if counter < len(plan)-1: new_room = room_gen(plan[-2], room_size, plan.pop_back())
+				else: new_room = room_gen(plan[-1], room_size)
+				
+				if new_room:
+					plan.append(new_room)
+					counter += 1
+				else:
+					counter -= 1
 			"exit":
-				if (plan[-1]["type"] == "line" and plan[-1]["direction"] == "south"):
-					instructions.insert(instructions.size()-1, "hall")
-					total_shapes += 1
-					print("added hall to exit")
-					continue
+				#if (plan[-1]["type"] == "line" and plan[-1]["direction"] == "south"):
+					#instructions.insert(instructions.size()-1, "hall")
+					#total_shapes += 1
+					#print("added hall to exit")
+					#continue
 				
 				counter += 1
 				continue
@@ -199,18 +245,24 @@ func gen_main_hall(num_rooms=Vector2(2,4), num_halls=Vector2(1,2), hall_length=V
 	print(instructions)
 	print("Took " + str(tries) + " tries")
 
+
 func trigger_gen():
-	#var rect1 = Rect2(-2, 2, 0, 5)
-	#var rect2 = Rect2(-1, 3, 0, 3)
+	#var rect1 = Rect2(0, -1, 0, 2)
+	#var rect2 = Rect2(0, -1, 1, 4)
 	#print(rect1)
 	#print(rect2)
 	#print(rect1.intersects(rect2, true))
 
-	gen_main_hall(Vector2(1,1), Vector2(9,12), Vector2(3,5))
+	gen_main_hall(Vector2(1,1), Vector2(3, 5), Vector2(4,6), Vector2(3,5))
 
 	print("------------------")
 	for object in plan:
 		print(object["type"], " ", object["direction"], object["start"], object["end"], object["rect"])
+		#if object["type"] == "line":
+			#if object["start"] !=  object["parent"]["end"]:
+				#print("cought you")
+			#else:
+				#print(object["start"], object["parent"]["end"])
 	#print(plan[-1])
 	print(len(plan))
 	print("done")
